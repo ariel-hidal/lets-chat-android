bd9a538d551d60f16613872cf3a5fe816e18ca66
ariel-hidal <arielhidal@gmail.com>
2017-03-20 17:50:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
     if (navigator.connection.type == Connection.NONE) {
       navigator.notification.alert('An internet connection is required to continue');
     } else {
-      window.location="http://www.myapp.com";
+      window.location="scholarchat-test.herokuapp.com";
     }
   }
   document.addEventListener("deviceready", onDeviceReady, false);
